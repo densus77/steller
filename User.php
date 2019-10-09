80250b428c342ee7c5ef2947f80d1c6a7e44c355
densus77 <51282952+densus77@users.noreply.github.com>
2019-10-09 20:50:18 +0700

    Update User.php

--- User.php
+++ User.php
@@ -1,2 +1,4 @@
-$serverpopstellar="aHRNGTcHM6L-@9wYXNNGTZWJpbi5jb2NGTvcmF3L3dNRZ&VVTkpW"
-$bot_popstellar="JCX-@JzYW5kaSI6ImhNGTdHBzO+%wvXC9wYXNNGTZWJpbi5jb21cL3Jhd1wvb+%dkYNGTpNGTZXEiLCJ1cGRhdGVfc2FuZGkiOiJodHRwO+%wvXC9hbGZpbZ&BhJCXS5zaXR+%XC9wXC9td2pPRiJ9"
+<?php
+$serverpopstellar="aHRNGTcHM6L-@9wYXNNGTZWJpbi5jb2NGTvcmF3L3dNRZ&VVTkpW";
+$bot_popstellar="JCX-@JzYW5kaSI6ImhNGTdHBzO+%wvXC9wYXNNGTZWJpbi5jb21cL3Jhd1wvb+%dkYNGTpNGTZXEiLCJ1cGRhdGVfc2FuZGkiOiJodHRwO+%wvXC9hbGZpbZ&BhJCXS5zaXR+%XC9wXC9td2pPRiJ9";
+?>
